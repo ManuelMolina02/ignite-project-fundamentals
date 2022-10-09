4579da502bd9dce724358f37e143e3c09bc6ccd2
Manuel Molina <manuelanmolina@gmail.com>
2022-10-09 13:32:20 -0300

    inserindo estilizacoes no sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { Header } from "./components/Header"
-import './global.css';
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import "./global.css";
+import styles from "./styles/App.module.css";
 
 function App() {
   return (
-    <Header/>
-  )
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>posts</main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1664575196851-5318f32c3f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50"
+        alt="Fundo de tela do perfil"
+      />
+
+      <div className={styles.profile}>
+        <img src="https://avatars.githubusercontent.com/u/63245910?v=4" />
+        <strong>Manuel Molina</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
